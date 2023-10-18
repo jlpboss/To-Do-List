1f48f82ae346f260a67f5255899f6b6c09b8d7a8
JLPBoss <jettpennington@gmail.com>
2023-10-18 16:55:27 -0400

    Day 1 done

--- to-do/app/Components/TaskList/task.js
+++ to-do/app/Components/TaskList/task.js
@@ -2,19 +2,21 @@ export default function Task(props){
     return(
         <div className="card">
             <div className="card-body">
-                <input 
-                    type="checkbox" 
-                    id={props.id} 
-                    checked={props.isCompleated ? "checked" : ""}
-                    onChange={props.checkHandler} 
-                />
-                <p className="card-text">{props.task}</p>
-                <button 
-                    id={props.id}
-                    onClick={props.onButtonClick}
-                >
-                    X
-                </button>
+                <div class="d-flex flex-row">
+                    <input 
+                        type="checkbox" 
+                        id={props.id} 
+                        checked={props.isCompleated ? "checked" : ""}
+                        onChange={props.checkHandler} 
+                    />
+                    <p className="card-text">{props.task}</p>
+                    <button 
+                        id={props.id}
+                        onClick={props.onButtonClick}
+                    >
+                        X
+                    </button>
+                </div>
             </div>
         </div>
     )
